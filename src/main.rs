8361c25779f94ab4c94c1b7a7dcce6e94ff1199d
Mikhail Shomov <m@shomov.spb.ru>
2022-12-02 22:37:37 +0300

    Improvements of verilog analysis

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,11 @@ fn main() -> Result<(), Error> {
     Handle::from_path(path_to_read)?;
     let mut module_name : String;
     let mut inputs: HashMap<String, i32> = HashMap::new();
-
-    
+    let mut outputs: HashMap<String, i32> = HashMap::new();
+    let mut wires: HashMap<String, i32> = HashMap::new();
+    let mut regs: HashMap<String, i32> = HashMap::new();
     let mut cont_assigns: HashMap<String, i32> = HashMap::new();
+    let mut always_assigns: HashMap<String, String> = HashMap::new();
 
     
     let file = File::open(&path_to_read)?;
@@ -27,10 +29,32 @@ fn main() -> Result<(), Error> {
         token.push(liner);
 
         let premodule_name = verilog_analysis::get_module_name(&token[0]);
-        if premodule_name != "!".to_string() {
+        if premodule_name != "".to_string() {
             module_name = premodule_name;
-            println!("{}", module_name);
         }
+
+        let (input, dimension) = verilog_analysis::get_inputs(&token[0]);
+        if input != "".to_string() {
+            inputs.insert(input, dimension);
+        }
+
+        let (output, dimension) = verilog_analysis::get_outputs(&token[0]);
+        if output != "".to_string() {
+            outputs.insert(output, dimension);
+        }
+
+        let (wire, dimension) = verilog_analysis::get_wires(&token[0]);
+        if wire != "".to_string() {
+            wires.insert(wire, dimension);
+        }
+        
+        let (reg, dimension) = verilog_analysis::get_regs(&token[0]);
+        if reg != "".to_string() {
+            regs.insert(reg, dimension);
+        }
+
+        let (always_event, always_assign) = verilog_analysis::get_alwayses(&token[0]);
+        always_assigns.insert(always_event, always_assign);
 
         let (pre_signals, pre_lut_cmd) = verilog_analysis::get_assign(&token[0]);
         if pre_signals != ""{
--- src/verilog_analysis.rs
+++ src/verilog_analysis.rs
@@ -6,94 +6,104 @@ use std::io::{BufRead, BufReader};
 
 pub fn get_module_name(line_r : &String) -> String {
     if Regex::new(r"\s*module \w+\s*").unwrap().is_match(&line_r.to_string())  {
-        let regex = Regex::new(r"\bmodule\s+(\w+)").unwrap();
+        let regex = Regex::new(r"(\w+)\($").unwrap();
         let module: Vec<&str> = regex.find_iter(&line_r).map(|x| x.as_str()).collect();
-        return (module[0]).to_string();
+        return (module[0]).to_string().replace("(", "");
     }
-    return "!".to_string();
+    return "".to_string();
 }
-pub fn get_inputs(file : File) -> HashMap<String, i32> {
+pub fn get_inputs(line_r : &String) -> (String, i32) {
     let input_regex = Regex::new(r"(input( reg|wire)?\s+(\[[0-9]:[0-9]?\])?\s*(\w+),?)").unwrap();
-    let mut inputs: HashMap<String, i32> = HashMap::new();
-    let buf_file = BufReader::new(file);
     
-    for (num, line) in buf_file.lines().enumerate() {
-        let line_r = line.unwrap();
-        if input_regex.is_match(&line_r.to_string()) {
-            let io_name: Vec<&str> = Regex::new(r"([a-z]+,?)$").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
+    if input_regex.is_match(&line_r.to_string()) {
+        let io_name: Vec<&str> = Regex::new(r"([a-z]+,?)$").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
 
-            if Regex::new(r"(\[[0-9]:[0-9]?\])").unwrap().is_match(&line_r.to_string()) {
-                let dimension: Vec<&str> = Regex::new(r"\d").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
-                inputs.insert(
-                    io_name[0].to_string().replace(",", ""),
-                    dimension[0].parse::<i32>().unwrap() + 1
-                );
-            }
-            else {
-                inputs.insert(
-                    io_name[0].to_string().replace(",", ""),
-                    1
-                );
-            }
+        if Regex::new(r"(\[[0-9]:[0-9]?\])").unwrap().is_match(&line_r.to_string()) {
+            let dimension: Vec<&str> = Regex::new(r"\d").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
+            return (
+                io_name[0].to_string().replace(",", ""),
+                dimension[0].parse::<i32>().unwrap() + 1
+            );
         }
+        else {
+            return(
+                io_name[0].to_string().replace(",", ""),
+                1
+            );
+        }
+    
     }
-    return inputs;
+    return ("".to_string(), 0);
 }
 
-pub fn get_wires(file : File) -> HashMap<String, i32> {
+pub fn get_outputs(line_r : &String) -> (String, i32) {
+    let output_regex = Regex::new(r"(output( reg|wire)?\s+(\[[0-9]:[0-9]?\])?\s*(\w+),?)").unwrap();
+    
+    if output_regex.is_match(&line_r.to_string()) {
+        let io_name: Vec<&str> = Regex::new(r"([a-z]+,?)$").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
+
+        if Regex::new(r"(\[[0-9]:[0-9]?\])").unwrap().is_match(&line_r.to_string()) {
+            let dimension: Vec<&str> = Regex::new(r"\d").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
+            return (
+                io_name[0].to_string().replace(",", ""),
+                dimension[0].parse::<i32>().unwrap() + 1
+            );
+        }
+        else {
+            return(
+                io_name[0].to_string().replace(",", ""),
+                1
+            );
+        }
+    
+    }
+    return ("".to_string(), 0);
+}
+
+pub fn get_wires(line_r : &String) -> (String, i32) {
     let wire_regex = Regex::new(r"^(wire?\s+(\[[0-9]:[0-9]?\])?\s*(\w+),?)+;").unwrap();
-    let mut wires: HashMap<String, i32> = HashMap::new();
-    let buf_file = BufReader::new(file);
-    
-    for (num, line) in buf_file.lines().enumerate() {
-        let line_r = line.unwrap();
-        if wire_regex.is_match(&line_r.to_string()) {
-            let wire_name: Vec<&str> = Regex::new(r"([a-z]+,?;?)$").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
+    if wire_regex.is_match(&line_r.to_string()) {
+        let wire_name: Vec<&str> = Regex::new(r"([a-z]+,?)$").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
 
-            if Regex::new(r"(\[[0-9]:[0-9]?\])").unwrap().is_match(&line_r.to_string()) {
-                let dimension: Vec<&str> = Regex::new(r"\d").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
-                wires.insert(
-                    wire_name[0].to_string().replace(",", ""),
-                    dimension[0].parse::<i32>().unwrap() + 1
-                );
-            }
-            else {
-                wires.insert(
-                    wire_name[0].to_string().replace(",", ""),
-                    1
-                );
-            }
+        if Regex::new(r"(\[[0-9]:[0-9]?\])").unwrap().is_match(&line_r.to_string()) {
+            let dimension: Vec<&str> = Regex::new(r"\d").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
+            return (
+                wire_name[0].to_string().replace(",", ""),
+                dimension[0].parse::<i32>().unwrap() + 1
+            );
         }
+        else {
+            return(
+                wire_name[0].to_string().replace(",", ""),
+                1
+            );
+        }
+    
     }
-    return wires;
+    return ("".to_string(), 0);
 }
 
-pub fn get_regs(file : File) -> HashMap<String, i32> {
+pub fn get_regs(line_r : &String) -> (String, i32) {
     let reg_regex = Regex::new(r"^(reg?\s+(\[[0-9]:[0-9]?\])?\s*(\w+),?)+;").unwrap();
-    let mut regs: HashMap<String, i32> = HashMap::new();
-    let buf_file = BufReader::new(file);
-    
-    for (num, line) in buf_file.lines().enumerate() {
-        let line_r = line.unwrap();
-        if reg_regex.is_match(&line_r.to_string()) {
-            let reg_name: Vec<&str> = Regex::new(r"([a-z]+,?;?)$").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
+    if reg_regex.is_match(&line_r.to_string()) {
+        let reg_name: Vec<&str> = Regex::new(r"([a-z]+,?)$").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
 
-            if Regex::new(r"(\[[0-9]:[0-9]?\])").unwrap().is_match(&line_r.to_string()) {
-                let dimension: Vec<&str> = Regex::new(r"\d").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
-                regs.insert(
-                    reg_name[0].to_string().replace(",", ""),
-                    dimension[0].parse::<i32>().unwrap() + 1
-                );
-            }
-            else {
-                regs.insert(
-                    reg_name[0].to_string().replace(",", ""),
-                    1
-                );
-            }
+        if Regex::new(r"(\[[0-9]:[0-9]?\])").unwrap().is_match(&line_r.to_string()) {
+            let dimension: Vec<&str> = Regex::new(r"\d").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
+            return (
+                reg_name[0].to_string().replace(",", ""),
+                dimension[0].parse::<i32>().unwrap() + 1
+            );
         }
+        else {
+            return(
+                reg_name[0].to_string().replace(",", ""),
+                1
+            );
+        }
+    
     }
-    return regs;
+    return ("".to_string(), 0);
 }
 
 pub fn get_assign(line_r : &String) -> (String, i32) {
@@ -120,40 +130,27 @@ pub fn get_assign(line_r : &String) -> (String, i32) {
     return ("".to_string(), 0);
 }
 
-pub fn get_alwayses(file : File) -> HashMap<String, i32> {
+pub fn get_alwayses(line_r : &String) -> (String, String) {
     let always_regex = Regex::new(r"^\s*(always(_ff)? @\((pos|neg)edge \w+\) begin\s*)").unwrap();
-    let mut alw_assignments: HashMap<String, i32> = HashMap::new();
-    let buf_file = BufReader::new(file);
-    
+    let mut event : String = "".to_string();
     let mut always_detect:bool = false;
 
-    for (num, line) in buf_file.lines().enumerate() {
-        let line_r = line.unwrap();
-        if always_regex.is_match(&line_r.to_string()) {
-            always_detect = true;
-        }
-        else if always_detect && Regex::new(r"^\s*end\s*").unwrap().is_match(&line_r.to_string()){
-            always_detect = false;
-        }
-        else if always_detect{
-            if Regex::new(r"^\s*((\w+ <?= \w+\s+[&|+-]+\s+\w+;\s?)|(\w+ [&|+-]+= \w+))").unwrap().is_match(&line_r.to_string()) {
-                let signals: Vec<&str> = Regex::new(r"\w+ <?= \w+\s+[&|+-]\s+\w+").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
-                let operation: Vec<&str> = Regex::new(r"[&|+-]+").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
-                let lut_cmd: i32 = match operation[0] {
-                    "&&" => 6,
-                    "||" => 5,
-                    "&" => 4,
-                    "|" => 3,
-                    "+" => 2,
-                    "-" => 1,
-                    _   => 0
-                };
-                alw_assignments.insert(
-                    signals[0].to_string(),
-                    lut_cmd
-                );
-            }
+    if !always_detect && always_regex.is_match(&line_r.to_string()) {
+        event = Regex::new(r"edge\s+(\w+)\s*").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
+        always_detect = true;
+    }
+    else if always_detect && Regex::new(r"^\s*end\s*").unwrap().is_match(&line_r.to_string()){
+        always_detect = false;
+    }
+    else if always_detect{
+        if Regex::new(r"^\s*((\w+ <?= \w+\s+[&|+-]+\s+\w+;\s?)|(\w+ [&|+-]+= \w+))").unwrap().is_match(&line_r.to_string()) {
+            let signals: Vec<&str> = Regex::new(r"\w+ <?= \w+\s+[&|+-]\s+\w+").unwrap().find_iter(&line_r).map(|x| x.as_str()).collect();
+            return (
+                event.replace("edge", ""),
+                signals[0].to_string(),
+            );
+        
         }
     }
-    return alw_assignments;
+    return ("".to_string(), "".to_string());
 }
